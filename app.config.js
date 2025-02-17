82eca1238bb83cbb381c6cc9f9a6dc485b2d87b3
Kapil619 <kapilbadokar321@gmail.com>
2025-02-17 14:09:59 +0530

    Created dynamic appconfig, And reading env from extras field

---
+++ app.config.js
@@ -0,0 +1,47 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "SmartPlug",
+    slug: "SmartPlug",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      package: "com.smartplug.smartplug",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "03cec3bf-8774-4987-b855-34159a75632b",
+      },
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    updates: {
+      url: "https://u.expo.dev/03cec3bf-8774-4987-b855-34159a75632b",
+    },
+  },
+};
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,13 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from "expo-constants";
+
+const extra = Constants.expoConfig?.extra as {
+    firebaseApiKey: string;
+    firebaseAuthDomain: string;
+    firebaseProjectId: string;
+    firebaseStorageBucket: string;
+    firebaseMessagingSenderId: string;
+    firebaseAppId: string;
+};
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: extra.firebaseApiKey,
+    authDomain: extra.firebaseAuthDomain,
+    projectId: extra.firebaseProjectId,
+    storageBucket: extra.firebaseStorageBucket,
+    messagingSenderId: extra.firebaseMessagingSenderId,
+    appId: extra.firebaseAppId,
 };
 
 // Initialize Firebase
